1daf1c02c1a4a61c4e72005b59fe165de885a639
dannny <nguyendanny0@gmail.com>
2020-12-28 17:38:41 -0800

    first commit

---
+++ src/main/java/mysqlapp/MysqlappApplication.java
@@ -0,0 +1,13 @@
+package mysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mysqlapp/MysqlappApplicationTests.java
@@ -0,0 +1,13 @@
+package mysqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
